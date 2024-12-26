8285b60d277bb282f32de405a168815063edbc3c
Arianna Giaccari <ariggiaccari@gmail.com>
2024-12-26 15:36:58 +0100

    login & autentication

--- login.php
+++ login.php
@@ -1,4 +1,45 @@
 <!-- gestione login e autenticazione -->
 <?php
-echo 'benvenuto nella pagina login';
+session_start();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {   // this checks if the module has been sent with the method 'post'
+    if ($_POST['username'] === 'admin' && $_POST['password'] === 'password') {
+        $_SESSION['logged_in'] = true;
+        header('Location: admin.php'); // if credentials are right, this sends you to the page dedicated to users 
+        exit;
+    } else {
+        $error = 'incorrect credentials!';
+    }
+}
 ?>
+<!-- $_SERVER  $_POST $_SESSION are associative arrays.
+$_SERVER contains data related to the server and the http request 
+$_POST contains data sent through the html module with post method
+$_SESSION  memorizes data for the user session. keeps the info between pages, like user identification -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <div>
+        <h2>Login</h2>
+        <form method="post">
+            <label for="username">username:</label>
+            <input type="text" name="username">
+            <label for="password">password:</label>
+            <input type="password" name="password">
+            <button type="submit">login</button>
+        </form>
+    </div>
+    <!-- shows the error message if credentials are wrong -->
+    <?php if (isset($error)) // isset($var) is a function that verifies if a var was defined and is not null
+            echo "<p> $error </p>"
+                ?>
+    </body>
+
+    </html>
