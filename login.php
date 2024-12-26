7565e9c69addc3be8c983e7310c1b7832a979cbe
Arianna Giaccari <ariggiaccari@gmail.com>
2024-12-26 19:23:46 +0100

    style added for the main page

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<!-- gestione login e autenticazione -->
+<!-- handle login & autentication -->
 <?php
 session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {   // this checks if the module has been sent with the method 'post'
@@ -22,6 +22,7 @@ $_SESSION  memorizes data for the user session. keeps the info between pages, li
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Login</title>
 </head>
 
